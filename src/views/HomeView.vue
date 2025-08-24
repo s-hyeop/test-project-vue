c8794894f917f7c2eb105f59f2a83bb62fa77008
song-laptop <tmdguq0330@gmail.com>
2025-08-24 19:31:18 +0900

    임시 home 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
   <div>HomeView</div>
+  <router-link v-if="!authStore.isAuth" to="/auth">비회원 [바로가기]</router-link>
+  <router-link v-if="authStore.isAuth" to="/todos">회원 [바로가기]</router-link>
 </template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+const authStore = useAuthStore;
+</script>
