95fe4952bcd613c51594806718b684b0a9a9deb9
song-laptop <tmdguq0330@gmail.com>
2025-08-24 15:19:42 +0900

    임시 헤더 상태별 접근 가능 메뉴 표기

--- src/components/_layouts/TheHeader.vue
+++ src/components/_layouts/TheHeader.vue
@@ -3,13 +3,17 @@
 
   <ul>
     <li><Router-Link to="/">Home</Router-Link></li>
-    <li><Router-Link to="/auth">auth</Router-Link></li>
-    <li><Router-Link to="/login">login</Router-Link></li>
-    <li><Router-Link to="/signup">signup</Router-Link></li>
-    <li><Router-Link to="/reset-password">reset-password</Router-Link></li>
-    <li><Router-Link to="/todos">todos</Router-Link></li>
-    <li><Router-Link to="/my-page">my-page</Router-Link></li>
+    <li v-if="!authStore.isAuth"><Router-Link to="/auth">auth</Router-Link></li>
+    <li v-if="!authStore.isAuth"><Router-Link to="/login">login</Router-Link></li>
+    <li v-if="!authStore.isAuth"><Router-Link to="/signup">signup</Router-Link></li>
+    <li v-if="!authStore.isAuth"><Router-Link to="/reset-password">reset-password</Router-Link></li>
+    <li v-if="authStore.isAuth"><Router-Link to="/todos">todos</Router-Link></li>
+    <li v-if="authStore.isAuth"><Router-Link to="/my-page">my-page</Router-Link></li>
   </ul>
 </template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+</script>
