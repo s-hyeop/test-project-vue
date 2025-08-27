56e7b896fd1fdb7fc0f6ef1efbdbe381939b1939
song-laptop <tmdguq0330@gmail.com>
2025-08-27 19:01:18 +0900

    버블링 제어

--- src/components/todos/TodoItem.vue
+++ src/components/todos/TodoItem.vue
@@ -2,9 +2,9 @@
   <div @click="$emit('detail')">
     <div>TodoItem</div>
     <div>{{ todo }}</div>
-    <input type="checkbox" :checked="!!todo.completedAt" @change="$emit('toggle', !todo.completedAt)" />
-    <button @click="$emit('update')">수정</button>
-    <button @click="$emit('delete')">삭제</button>
+    <input type="checkbox" :checked="!!todo.completedAt" @click.stop="" @change="$emit('toggle', !todo.completedAt)" />
+    <button @click.stop="$emit('update')">수정</button>
+    <button @click.stop="$emit('delete')">삭제</button>
   </div>
   <hr />
 </template>
